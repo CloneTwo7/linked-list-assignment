1af88ba98fe51371b2fe4144c4083f42d6eb012f
OrdinaryOwlbear <travisp0027@gmail.com>
2020-02-02 12:25:50 -0800

    Added comments to add.c

--- add.c
+++ add.c
@@ -13,31 +13,39 @@
 
 int add(struct node* LL, int num) {
 
+	//creates variable "exists" to test whether or not the node alread exists
 	int exists = search(LL, num);
-
+	//if it does, then exit function with value 0
 	if(exists == true) {
 		return (0);
 	}
 	
+	//if value does not exist, create a node called "new"
 	struct node *new = malloc(sizeof(struct node*));
 
+	//create struct node* for previous node and current node
 	struct node *prev = LL; //sets previous to be the sentinel node
 	struct node *current = LL -> next; //sets current to be the next node in the LL (possible NULL)
 
+	//created flag variable called adding to see when it is done adding
 	int adding = true;
 	while(adding == true) {
+		//if current == NULL then it adds the node to the end of the LL
 		if(current == NULL) {
 			new -> data = num;
 			new -> next = NULL;
 			prev -> next = new;
 			
+			//returns 1 if added successfully
 			return 1;
 		}
+		//if current's data field is greater than the number being added, adds the node between previous and current
 		if(current -> data > num) {
 			new -> data = num;
 			new -> next = current;
 			prev -> next = new;
 			
+			//returns 1 if added successfully
 			return 1;
 		}
 		else {
